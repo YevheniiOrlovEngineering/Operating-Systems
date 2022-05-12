5bdce6a3467b7d8ef9e0868d6acf16fcbad996b8
Yevhenii Orlov <orlovowork@gmail.com>
2022-05-12 04:15:05 +0300

    Added driver for SJF

--- lab-1/main.go
+++ lab-1/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/YevheniiOrlovEngineering/Operating-Systems/lab-1/algorithms"
 	"github.com/YevheniiOrlovEngineering/Operating-Systems/lab-1/utils"
 )
 
 // main drives SJF algorithm
 func main() {
-	pArr := utils.GenerateProc()
-	utils.PrintProcess(pArr)
+	pList := utils.GenerateProc()
+	utils.PrintTableStdOut(pList, "Arriving Order")
+	doneP := algorithms.SJF(pList)
+	utils.PrintTableStdOut(doneP, "SJF Order")
 }
